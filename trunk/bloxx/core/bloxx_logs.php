fde969b5b2d1879d9092f7c16e86a7cf61733f0f
secretdraft <secretdraft@55b00131-9232-4634-8403-66eb5d6af5e9>
2005-02-25 05:27:45 +0000

    doRender added
    git-svn-id: file:///Users/telmo/projects/bloxx_cvs/svnrepo@49 55b00131-9232-4634-8403-66eb5d6af5e9

--- trunk/bloxx/core/bloxx_logs.php
+++ trunk/bloxx/core/bloxx_logs.php
@@ -19,7 +19,7 @@
 //
 // Authors: Silas Francisco <draft@dog.kicks-ass.net>
 //
-// $Id: bloxx_logs.php,v 1.1 2005-02-25 03:29:14 secretdraft Exp $
+// $Id: bloxx_logs.php,v 1.2 2005-02-25 05:27:45 secretdraft Exp $
 
 require_once(CORE_DIR . 'bloxx_module.php');
 
@@ -27,7 +27,7 @@ require_once(CORE_DIR . 'bloxx_module.php');
  * Bloxx_Logs Implements log system.
  *
  * @package   Bloxx_Core
- * @version   $Id: bloxx_logs.php,v 1.1 2005-02-25 03:29:14 secretdraft Exp $
+ * @version   $Id: bloxx_logs.php,v 1.2 2005-02-25 05:27:45 secretdraft Exp $
  * @category  Core
  * @copyright Copyright &copy; 2002-2005 The Bloxx Team
  * @license   The GNU General Public License, Version 2
@@ -47,7 +47,7 @@ class Bloxx_Logs extends Bloxx_Module
 			$this->Bloxx_Module();	
 		}
 	
-       function getTableDefinition() 
+        function getTableDefinition() 
         {        
                 return array(
                 		'ownerModuleId' => array('TYPE' => 'NUMBER', 'SIZE' => -1, 'NOTNULL' => true),
@@ -56,6 +56,74 @@ class Bloxx_Logs extends Bloxx_Module
                         'priority'      => array('TYPE' => 'NUMBER', 'SIZE' => -1, 'NOTNULL' => true),
                         'message'       => array('TYPE' => 'STRING', 'SIZE' => 256, 'NOTNULL' => true));
         }
+
+		function doRender($mode, $id, $target, $mt)
+		{
+			include_once(CORE_DIR . 'bloxx_form.php');
+			
+			if ($mode == 'logs')
+			{
+				$form = new Bloxx_Form();
+				$html_out = $form->renderHeader($this->name, 'view');
+				$mt->setItem('header', $html_out);
+				
+				$html_out = $form->startSelect('priority', '1');
+				$mt->setItem('startselect', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_EMERG, 'LOG_EMERG');
+				$mt->setItem('option0', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_ALERT, 'LOG_ALERT');
+				$mt->setItem('option1', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_CRIT, 'LOG_CRIT');
+				$mt->setItem('option2', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_ERR, 'LOG_ERR');
+				$mt->setItem('option3', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_WARNING, 'LOG_WARNING');
+				$mt->setItem('option4', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_NOTICE, 'LOG_NOTICE');
+				$mt->setItem('option5', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_INFO, 'LOG_INFO');
+				$mt->setItem('option6', $html_out);
+				
+				$html_out = $form->addSelectItem(LOG_DEBUG, 'LOG_DEBUG');
+				$mt->setItem('option7', $html_out);
+				
+				$html_out = $form->endSelect();
+				$mt->setItem('endselect', $html_out);
+				
+				$html_out = $form->renderSubmitButton('Show');
+				$mt->setItem('show', $html_out);
+				
+				$html_out = $form->renderFooter();
+				$mt->setItem('footer', $html_out);
+				
+				if (isset($_POST['priority']))
+				{
+					$this->clearWhereCondition();
+					$this->insertWhereCondition('priority', '<=', $_POST['priority']);
+					$this->runSelect();
+					
+					$html_out = '';
+					
+					while ($this->nextRow())
+					{
+						$html_out .= $this->timestamp . ' ' . $this->ownerModuleId . ': '
+							. '[' . $this->addr . '] ' . $this->priority . ' ' . $this->message . "\n";
+					}
+					
+					$mt->setItem('logs', $html_out);
+				}				
+				
+				return $mt->renderView();
+			}
+			
+		}
         		
 		/**
 		 * writeLog Logs a message.
